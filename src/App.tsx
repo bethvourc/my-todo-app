54092b8f6cb301a7e24268b63c196bacee4a4e4a
Bethvour <bethvourc@gmail.com>
2024-06-02 17:10:52 -0700

    App icon update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="w-full max-w-lg">
         <TodoList />
       </div>
